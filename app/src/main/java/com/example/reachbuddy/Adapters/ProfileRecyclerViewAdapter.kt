3274581eaad1462df613e776dbc4d89a20af77a0
atif9 <mohammadatif92080@gmail.com>
2021-04-08 23:10:51 +0530

    documentated ProfilerecyclerviewAdapter

--- app/src/main/java/com/example/reachbuddy/Adapters/ProfileRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/reachbuddy/Adapters/ProfileRecyclerViewAdapter.kt
@@ -14,12 +14,19 @@ import com.example.reachbuddy.R
 class ProfileRecyclerViewAdapter : RecyclerView.Adapter<ProfileRecyclerViewAdapter.ViewHolder>() {
 
     private var listofProfiles:MutableList<UserProfile> = mutableListOf<UserProfile>()
+
+    /*
+    Everytime you create a recyclerview adapter you need to create a custom viewHolder class
+    that describes what the views looking like in the recyclerview
+    It just describles all the views in the recyclerview
+     */
     inner class ViewHolder(view: View):RecyclerView.ViewHolder(view)
     {
         val profilepic : ImageView =view.findViewById(R.id.ProfilePicImg)
         val profilename:TextView=view.findViewById(R.id.DisplayNameTxt)
     }
 
+    //this fucntion is to update the list when new pofile are added or profiles are deleted
     fun updatelist(list: MutableList<UserProfile>)
     {
         Log.e("got called","RecyclerView Profile Funtion")
@@ -27,6 +34,9 @@ class ProfileRecyclerViewAdapter : RecyclerView.Adapter<ProfileRecyclerViewAdapt
         notifyDataSetChanged()
     }
 
+    /*
+    This method is responsible for creating each on of viewholders which are displayed inside the recyclerview to user
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.profilecardview,parent,false)
@@ -37,6 +47,11 @@ class ProfileRecyclerViewAdapter : RecyclerView.Adapter<ProfileRecyclerViewAdapt
         return listofProfiles.size
     }
 
+    /*
+    This function is to bind the data to the viewholder that is currently is in view
+    This binds/assigns the data to the viewHolder
+    Whenenver user scrolls new viewHolder Comes and This methods is called
+     */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.profilename.text=listofProfiles.get(position).UserName
         Glide.with(holder.itemView).load(listofProfiles.get(position).UserProfilePicLink).circleCrop().into(holder.profilepic)
